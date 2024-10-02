a375381b333585877689afdd4fa206ff8fa01c02
Piggy <piggynl@outlook.com>
2024-10-02 16:49:40 +0800

    build: add vike-cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import vike from "vike/plugin";
+import { pages } from "vike-cloudflare";
 
 export default defineConfig({
-  plugins: [vike({}), react({})],
+  plugins: [
+    vike({}),
+    react({}),
+    pages({})
+  ],
 });
